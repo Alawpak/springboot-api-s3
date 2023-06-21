a33f2df865be575dd7001a32f1b41a6442d6a524
Alawpak <alan281@hotmail.es>
2023-06-21 12:55:23 -0600

    first commit

---
+++ src/main/java/com/springbootapiawss3/SpringbootApiAwsS3Application.java
@@ -0,0 +1,13 @@
+package com.springbootapiawss3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiAwsS3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApiAwsS3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/springbootapiawss3/SpringbootApiAwsS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootapiawss3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiAwsS3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
